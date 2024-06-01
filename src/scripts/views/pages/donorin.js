5ba16a33a0ee5edaa1455e3ba6932d2ab270d17f
Mildan Falah <mildanfalah@gmail.com>
2024-06-01 15:16:11 +0700

    menambahkan style responsive dan penyesuaian untuk homepage, donorin dan profile

--- src/scripts/views/pages/donorin.js
+++ src/scripts/views/pages/donorin.js
@@ -8,35 +8,37 @@ const Donorin = {
   async render() {
     return `       
         <div class="top-content">
-        <div class="search-bar">
-          <input type="text" placeholder="Cari kota...">
-          <button type="submit"><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
-        </div>
-        <div class="selection-content">
-          <div class="tagline-content">
-            <div class="tagline-clip">
-              <div class="tagline-image">
-                <div class="background-image"></div>
-                <img src="./images/white1.png" />
+          <div class="top-content-wrapper">
+              <div class="search-bar">
+                <input type="text" placeholder="Cari kota...">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+              </div>
+              <div class="selection-content">
+                <div class="tagline-content">
+                  <div class="tagline-clip">
+                    <div class="tagline-image">
+                      <div class="background-image"></div>
+                      <img src="./images/white1.png" />
+                    </div>
+                  </div>
+                <div class="tagline-text">
+                  <p>Setetes Darah, Sejuta Harapan</p>
+                </div>
+              </div>
+              <p class="selection-question">Apakah anda?</p>
+              <div class="selection-button-container">
+                <p id="pendonor" class="selection-button pendonor-button button-selected">Pendonor</p>
+                <p id="penerima" class="selection-button penerima-button">Penerima</p>
+              </div>
+              <div id="pendonorDescription" class="description-bar show">
+                <p>Halaman pendonor untuk menampilkan penerima donor</p>
+              </div>
+              <div id="penerimaDescription" class="description-bar">
+                <p>Halaman penerima donor untuk menampilkan pendonor</p>
               </div>
             </div>
-            <div class="tagline-text">
-              <p>Setetes Darah, Sejuta Harapan</p>
-            </div>
           </div>
-          <p class="selection-question">Apakah anda?</p>
-          <div class="selection-button-container">
-            <p id="pendonor" class="selection-button pendonor-button button-selected">Pendonor</p>
-            <p id="penerima" class="selection-button penerima-button">Penerima</p>
-          </div>
-          <div id="pendonorDescription" class="description-bar show">
-            <p>Halaman pendonor untuk menampilkan penerima donor</p>
-          </div>
-        <div id="penerimaDescription" class="description-bar">
-          <p>Halaman penerima donor untuk menampilkan pendonor</p>
         </div>
-      </div>
-    </div>
 
         <div class="donorin-list"></div>
           `;
