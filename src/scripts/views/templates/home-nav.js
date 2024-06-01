5ea59694037be3dabd9029b2ca0abded17001f6d
Mildan Falah <mildanfalah@gmail.com>
2024-06-01 19:54:03 +0700

    memperbaiki navbutton sehingga sesuai dengan page yang dibuka (sebelumnya ada bug)

--- src/scripts/views/templates/home-nav.js
+++ src/scripts/views/templates/home-nav.js
@@ -1,17 +1,38 @@
+import UrlParser from "../../routes/url-parser";
+
 const navButtonFunction = () => {
   const navButtons = document.querySelectorAll(".nav-button");
 
-  function selectItem(e) {
+  function removeSelection() {
+    navButtons.forEach((button) => {
+      button.classList.remove("nav-active");
+    });
+  }
+
+  function addActiveClassToCurrentPage() {
+    const { resource } = UrlParser.parseActiveUrlWithoutCombiner();
+    const currentPage = resource || "home";
+
     removeSelection();
 
+    navButtons.forEach((button) => {
+      const page = button.getAttribute("data-page");
+      if (page === currentPage) {
+        button.classList.add("nav-active");
+      }
+    });
+  }
+
+  function handleNavButtonClick() {
+    removeSelection();
     this.classList.add("nav-active");
   }
 
-  function removeSelection() {
-    navButtons.forEach((button) => button.classList.remove("nav-active"));
-  }
+  navButtons.forEach((button) =>
+    button.addEventListener("click", handleNavButtonClick)
+  );
 
-  navButtons.forEach((button) => button.addEventListener("click", selectItem));
+  addActiveClassToCurrentPage();
 };
 
 export default navButtonFunction;
